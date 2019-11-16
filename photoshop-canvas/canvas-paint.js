86bdc9776902e6d0e0b587f571a9dd187ad697aa
Szymon Gliwa <szymon.gliwa@microsoft.wsei.edu.pl>
2019-11-16 23:10:10 +0100

    2 new tools
    greyscale
    invert

--- photoshop-canvas/canvas-paint.js
+++ photoshop-canvas/canvas-paint.js
@@ -11,6 +11,12 @@ function appStart() {
     document
         .querySelector('#ligten')
         .addEventListener('click', () => ligtenFilter())
+    document
+        .querySelector('#invert')
+        .addEventListener('click', () => Invert())
+    document
+        .querySelector('#greyscale')
+        .addEventListener('click', () => GreyScale())
   ctx =canvas.getContext('2d')
     /*ctx.rect(50,50,300,200)
     ctx.fill()
@@ -43,6 +49,24 @@ function ligtenFilter(amount = 30) {
         canvasData.data[i+1] += amount
         canvasData.data[i+2] += amount
         //canvasData.data[i+3] -= amount
+    }
+    ctx.putImageData(canvasData, 0, 0)
+}
+function Invert(){
+    const canvasData = ctx.getImageData(0,0,200,300)
+    for (let i = 0; i < canvasData.data.length; i+=4){
+        canvasData.data[i] = 255 - canvasData.data[i]
+        canvasData.data[i + 1] = 255 - canvasData.data[i + 1]
+        canvasData.data[i + 2] = 255 - canvasData.data[i + 2]
+    }
+    ctx.putImageData(canvasData, 0, 0)}
+function GreyScale(){
+    const canvasData = ctx.getImageData(0,0,200,300)
+    for (let i=0; i < canvasData.data.length; i+=4){
+        let avg = (canvasData.data[i] + canvasData.data[i + 1] + canvasData.data[i + 2]) /3
+        canvasData.data[i] = avg
+        canvasData.data[i + 1] = avg
+        canvasData.data[i + 2] = avg
     }
     ctx.putImageData(canvasData, 0, 0)
 }
